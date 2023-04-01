6a52376b439e3bf6dd89fb1a84a0c8e21cfa6112
Alextran0899 <Alextran0899@gmail.com>
2023-03-31 23:59:51 -0500

    Initial Commit

---
+++ Recyclelit/RecyclelitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecyclelitApp.swift
+//  Recyclelit
+//
+//  Created by Alex Tran on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct RecyclelitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
